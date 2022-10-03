d2e295f5c2203f5bad55395eac09e19e1f80e541
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-10-03 19:57:08 +0300

    Добавляет решение второй задачи дз

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ulRef = document.querySelector('#ingredients')
+
+const liRef = ingredients.map(ingredient => {
+  const ingredientsRef = document.createElement('li')
+  ingredientsRef.classList.add('item');
+  ingredientsRef.textContent = ingredient;
+
+  return ingredientsRef
+})
+
+ulRef.append(...liRef)
