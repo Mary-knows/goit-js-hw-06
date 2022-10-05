8026d3ae173fa6e9819e8335be0efaa700719279
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-10-05 20:15:28 +0300

    Добавляет решение 9 задачи дз

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+
+const bgrColorRef = document.querySelector('.color');
+const btnColorRef = document.querySelector('.change-color');
+
+btnColorRef.addEventListener('click', onClickBtn);
+
+function onClickBtn(event) {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  bgrColorRef.textContent = newColor;
+}
+
