a049ba446a3cbe3053fef25eda9177211f156d75
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-10-05 19:29:23 +0300

    Добавляет решение 8 задачи дз

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    
+    if (email && password) {
+    
+        const formData = {
+            email,
+            password
+        }
+
+        console.log(formData)
+        event.target.reset()
+    } else {
+        alert('все поля должны быть заполнены')
+    }
+
+}
