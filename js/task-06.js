28ca066ac63e3e9a030bceba140844f1a9085a9b
Mariia Prokopenko <Klymenkomasha@gmail.com>
2022-10-08 18:17:30 +0300

    Исправляет 6 задачу дз

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,16 @@
 const formRef = document.querySelector('#validation-input');
-console.log(formRef)
 
 const inputLength = Number.parseInt(formRef.getAttribute('data-length'));
-console.log(inputLength)
 
 formRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
     const enterSymbols = event.currentTarget.value.length;
-    if (enterSymbols < inputLength) {
-        formRef.classList.add('invalid')
+    if (enterSymbols === inputLength) {
+        formRef.classList.add('valid');
+        formRef.classList.remove('invalid'); 
     } else {
-        formRef.classList.replace('invalid', 'valid')
+        formRef.classList.add('invalid');
+        formRef.classList.remove('valid');
     }
-
 }
